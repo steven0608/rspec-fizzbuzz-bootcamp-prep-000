efbf986a063516d7fb5024cbc9a63d07db2c3f78
Steven Chen <ysteven.chen2@gmail.com>
2018-04-12 05:56:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
 def fizzbuzz(x)
 if x % 3 == 0 && x % 5 == 0
 puts "FizzBuzz"
-elsif x % 3 == 0
+if x % 3 == 0
 puts "fizz"
-elsif x % 5 == 0
+if x % 5 == 0
   puts"Buzz"
-  end
 end
 fizzbuzz(15)
 fizzbuzz(3)
