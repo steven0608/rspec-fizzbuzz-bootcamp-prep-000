99c5d42f396f948c4f9d72235615d429b9ee0b7c
Steven Chen <ysteven.chen2@gmail.com>
2018-04-12 05:46:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(x)
-if x%3==0 && x%5==0
+def fizzbuzz(int)
+if int%3==0 && x%5==0
   puts "FizzBuzz"
-elsif x%3==0
+elsif int%3==0
     puts "fizz"
-  elsif x%5==0
+  elsif int%5==0
     puts "Buzz"
   else
     puts"nil"
